091f8aee37de10eb042d1951ed1185dd2b62463e
RominaCalani <calaniromina01@gmail.com>
2025-02-19 22:47:33 -0300

    subido

---
+++ src/utils/all.js
@@ -0,0 +1,37 @@
+import getReadingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
+
+/** Format Date */
+export const getFormattedDate = (date) =>
+  date
+    ? (() => {
+        const fecha = new Date(date);
+        
+        const mes = fecha.toLocaleDateString("es-ES", { month: "long" });
+        const dia = fecha.getDate();
+        const año = fecha.getFullYear();
+
+        return `${mes.charAt(0).toUpperCase() + mes.slice(1)} ${dia}, ${año}`;
+      })()
+    : "";
+    
+
+/** Estimated Reading time */
+export function remarkReadingTime() {
+  return function (tree, { data }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+
+    data.astro.frontmatter.estReadingTime = readingTime.minutes;
+  };
+}
+
+/** Check if an Image Path is Relative or Absolute */
+export const checkImageUrl = (image, url) => {
+  try {
+    new URL(image);
+    return image;
+  } catch (error) {
+    return new URL(image, url).toString();
+  }
+};
